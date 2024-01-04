d1993275839a286e7a5ab6b76586ce6008fcd6db
DD1C74707 <gracevalley101@gmail.com>
2024-01-04 11:19:53 -0800

    Fix pycode styles

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python
-#implements the pascal triangle
+'''implements the pascal triangle
+'''
+
 
 def pascal_triangle(n):
-	triangle = []
-	if n <= 0:
-		return []
-	else:
-		old_results = [1, 1]
-		for i in range(0, n):
-			if i == 0:
-				results = [1]
-			else:
-				results = [1]
-				counter = 0
-				for num in old_results:
-					if len(old_results) == 2 and len(results) == 2 and i > 1:
-						results.append(1)
-					elif i > 1 and counter != len(old_results) - 1:
-						results.append(num + old_results[counter + 1])
-					else:
-						if len(results) == 2 and i == 1:
-							pass
-						else:
-							results.append(1)
+    triangle = []
+    if n <= 0:
+        return []
+    else:
+        old_results = [1, 1]
+        for i in range(0, n):
+            if i == 0:
+                results = [1]
+            else:
+                results = [1]
+                counter = 0
+                for num in old_results:
+                    if len(old_results) == 2 and len(results) == 2 and i > 1:
+                        results.append(1)
+                    elif i > 1 and counter != len(old_results) - 1:
+                        results.append(num + old_results[counter + 1])
+                    else:
+                        if len(results) == 2 and i == 1:
+                            pass
+                        else:
+                            results.append(1)
 
-					counter += 1
-				old_results = results
-			triangle.append(results)
-	return triangle
-
+                    counter += 1
+                old_results = results
+        triangle.append(results)
+    return triangle

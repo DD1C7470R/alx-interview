eeea65396a9d4fb94b83d0ad250de81b9b58e5c7
DD1C7470R <gracevalley101@gmail.com>
2024-01-11 15:28:01 +0000

    Add script for locked boxes algorithm

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        # Empty input or the first box is empty
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True  # Mark the first box as visited
+
+    queue = deque([0])  # Start BFS with the first box
+
+    while queue:
+        current_box = queue.popleft()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
+

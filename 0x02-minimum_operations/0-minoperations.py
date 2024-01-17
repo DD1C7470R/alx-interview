638e6af74d77eec4c78fe19c594d204b80a44f82
DD1C7470R <gracevalley101@gmail.com>
2024-01-17 05:09:00 +0000

    Fix block

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,18 +9,18 @@ def minOperations(n):
     if n <= 1 or type(n) != int:
         return 0
     else:
-    if n <= 3:
-        return n
-    else:
-        num_of_letters = 3
-        while num_of_ops < n and num_of_letters < n:
-            if (num_of_letters * 2) <= n:
-                num_of_ops += 2
-                num_of_letters = num_of_letters * 2
-            else:
-                num_of_ops += 1
-                if n - num_of_letters < 3:
-                    num_of_letters += 1
+        if n <= 3:
+            return n
+        else:
+            num_of_letters = 3
+            while num_of_ops < n and num_of_letters < n:
+                if (num_of_letters * 2) <= n:
+                    num_of_ops += 2
+                    num_of_letters = num_of_letters * 2
                 else:
-                    num_of_letters += 3
+                    num_of_ops += 1
+                    if n - num_of_letters < 3:
+                        num_of_letters += 1
+                    else:
+                        num_of_letters += 3
     return num_of_ops

a7e1ef4c077ea602f6777f6db455f3e2f2f26940
DD1C7470R <gracevalley101@gmail.com>
2024-01-17 05:05:01 +0000

    Add test algorithm

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Returns the minimum number of operations"""
+    num_of_ops = 3
+    num_of_letters = 1
+    if n <= 1 or type(n) != int:
+        return 0
+    else:
+    if n <= 3:
+        return n
+    else:
+        num_of_letters = 3
+        while num_of_ops < n and num_of_letters < n:
+            if (num_of_letters * 2) <= n:
+                num_of_ops += 2
+                num_of_letters = num_of_letters * 2
+            else:
+                num_of_ops += 1
+                if n - num_of_letters < 3:
+                    num_of_letters += 1
+                else:
+                    num_of_letters += 3
+    return num_of_ops

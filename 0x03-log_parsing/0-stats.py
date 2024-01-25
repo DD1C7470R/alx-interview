ddf2188e84a79af94f7ecb6ab7a54e35cb4fdd28
DD1C7470R <gracevalley101@gmail.com>
2024-01-25 20:35:29 +0000

    Add an update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,14 +13,17 @@ def process_log_line(line):
         \s"GET\s/projects/260\sHTTP/1\.1"\s(\d+)\s(\d+)
         $
         ''', re.VERBOSE)
-    is_valid_line = pattern.match(line)
-    if not is_valid_line:
-        return None
-    # Extract relevant information from the match object
-    ip_address, date, status_code, file_size = is_valid_line.groups()
-    # Convert file_size to an integer
-    file_size = int(file_size)
-    # Return a dictionary with the extracted information
+    try:
+        is_valid_line = pattern.match(line)
+        if not is_valid_line:
+            return None
+        # Extract relevant information from the match object
+        ip_address, date, status_code, file_size = is_valid_line.groups()
+        # Convert file_size to an integer
+        file_size = int(file_size)
+    except BaseException:
+        pass
+        # Return a dictionary with the extracted information
     return {
             'ip_address': ip_address,
             'date': date, 'status_code': int(status_code),
@@ -28,7 +31,7 @@ def process_log_line(line):
             }
 
 
-def print_statistics(total_file_size, lines_by_status):
+def print_statistics(total_file_size, lines_by_status) -> None:
     print('File size: {:d}'.format(total_file_size))
     for status_code in sorted(lines_by_status):
         print('{}: {:d}'.format(status_code, lines_by_status[status_code]))
@@ -43,8 +46,11 @@ if __name__ == "__main__":
         for line in sys.stdin:
             log_entry = process_log_line(line)
             if log_entry:
-                total_file_size += log_entry['file_size']
-                lines_by_status[log_entry['status_code']] += 1
+                try:
+                    total_file_size += log_entry['file_size']
+                    lines_by_status[log_entry['status_code']] += 1
+                except BaseException:
+                    pass
                 line_count += 1
                 if line_count % 10 == 0:
                     print_statistics(total_file_size, lines_by_status)

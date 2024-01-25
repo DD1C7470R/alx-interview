44da4a4cadacada9c3126b7f4466b9563ef055e5
DD1C7470R <gracevalley101@gmail.com>
2024-01-25 21:21:26 +0000

    Add all requirements

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,26 +3,6 @@
 from collections import defaultdict
 
 
-def process_log_line(line):
-    '''Retrieves the necessary components from a logged file'''
-    try:
-        is_valid_line = line.split()
-        if not is_valid_line:
-            return None
-        # Extract relevant information from the match object
-        status_code = is_valid_line[-2]
-        file_size = is_valid_line[-1]
-        # Convert file_size to an integer
-        file_size = int(file_size)
-    except BaseException:
-        pass
-        # Return a dictionary with the extracted information
-    return {
-             'status_code': status_code,
-            'file_size': file_size
-            }
-
-
 def print_statistics(total_file_size, lines_by_status) -> None:
     print('File size: {:d}'.format(total_file_size))
     for status_code in sorted(lines_by_status):
@@ -35,7 +15,7 @@ if __name__ == "__main__":
     total_file_size = 0
     line_count = 0
     file_size = 0
-    codes =  ["200", "301", "400", "401", "403", "404", "405", "500"]
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     try:
         for line in sys.stdin:
             line_count += 1

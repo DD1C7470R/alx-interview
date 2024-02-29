6cebfec0a6df35021aea64fe372bcb971d093f15
DD1C7470R <gracevalley101@gmail.com>
2024-02-29 23:25:18 +0000

    Fix check 7

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,17 @@ Main file for testing
 
 def makeChange(coins: list, total: int) -> int:
     """Defines and implementations"""
+   if total <= 0:
+        return 0
 
-    if total < 0:
-        return -1
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
+
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+
+   if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
